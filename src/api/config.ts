7423c49d18658a8b8349a3ce65456539edd5522d
vlad-komanchy <vkomanchy@gmail.com>
2021-05-07 01:35:58 +0300

    feat: more logs for envs

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,5 @@
 export const API_HOST = process.env.MODE === 'production' ? process.env.PROD_API : process.env.DEV_API;
-console.log('API_HOST', API_HOST);
+
+console.log('API_HOST', API_HOST, 'MODE', process.env.MODE, 'PROD_API', process.env.PROD_API);
+
 export const HOST = 'http://localhost:8080/';
