f9654b70c9eee061760cf6d471f7c0894cae07b1
vlad-komanchy <vkomanchy@gmail.com>
2021-05-07 01:29:14 +0300

    feat: add log for API_HOST

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,3 @@
 export const API_HOST = process.env.MODE === 'production' ? process.env.PROD_API : process.env.DEV_API;
+console.log('API_HOST', API_HOST);
 export const HOST = 'http://localhost:8080/';
