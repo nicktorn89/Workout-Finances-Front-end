46056d0c1c3bff80b3698002690a33d01ec2f5ad
vlad-komanchy <vkomanchy@gmail.com>
2021-05-06 23:35:27 +0300

    feat: remove unneeded console.log

--- express.js
+++ express.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 const portNumber = 8086;
-const sourceDir = 'dist';
 
 app.use(express.static(sourceDir));
 
 app.listen(portNumber, () => {
   console.log(`Express web server started: http://localhost:${portNumber}`);
-  console.log(`Serving content from /${sourceDir}/`);
 });
